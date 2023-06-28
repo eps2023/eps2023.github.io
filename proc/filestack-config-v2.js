35eea6a4ea209f0b8570f7a2513d90b0863d0e8e
eps2023 <112888345+eps2023@users.noreply.github.com>
2023-06-28 15:51:00 +0200

    Ressources for proceedings

---
+++ proc/filestack-config-v2.js
@@ -0,0 +1,87 @@
+
+
+window.addEventListener('DOMContentLoaded', function () {
+
+  // My key to the filestack
+  const apikey = 'AwI8zeAXhScenP4YLcivGz';
+
+  // Init the client 
+  const client = filestack.init(apikey);
+
+  yo = client;
+
+  const onProgress = (evt) => {
+    document.getElementById('progress').innerHTML = `${evt.totalPercent}%`;
+  };
+
+  document.querySelector('input').addEventListener('change', (event) => {
+    
+    const files = event.target.files[0];
+    console.log(files)
+    const token = {};
+    const cancel = document.getElementById('cancel');
+    const pause = document.getElementById('pause');
+    const resume = document.getElementById('resume');
+
+    [cancel, resume, pause].forEach((btn) => {
+      const id = btn.id;
+      btn.addEventListener('click', () => {
+        token[id]();
+      });
+
+
+    });
+
+
+  
+
+
+//    client.upload(files, { onProgress }, {}, token)
+ //     .then(res => {
+   //     console.log('success: ', res)
+     // })
+     // .catch(err => {
+      //  console.log(err)
+     // });
+      
+  });
+});
+
+
+// FV : Open the file picker 
+function clickTest () {
+    console.log("click")
+
+    const apikey = 'AwI8zeAXhScenP4YLcivGz';
+    const client = filestack.init(apikey);
+
+    const options = {
+        maxFiles: 1,
+        uploadInBackground: false,
+        fromSources: ["local_file_system"],
+        accept: ["application/pdf"],
+      
+        onOpen: () => console.log('opened!'),
+        onUploadDone: (res) => {
+            //console.log(res);
+            //display = document.querySelector('.fileinfo');
+            document.getElementById("fileinfo").value = res.filesUploaded[0].filename + " uploaded successfully !";
+            console.log(res.filesUploaded[0].filename);
+        },
+      };
+
+      client.picker(options).open();
+  
+
+    //const files = event.target.files[0];
+    //const token = {};
+
+    //client.upload(files, { onProgress }, {}, token)
+     // .then(res => {
+      //  console.log('success: ', res)
+     // })
+     // .catch(err => {
+      //  console.log(err)
+      //});
+
+  }
